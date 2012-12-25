17395275340295db9f73f01ca41fd839b74576e2
Jose Antonio Corachán <jacorachan@gmail.com>
2012-12-25 20:42:55 +0100

    Improved recieved postData to display on form

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,6 @@
-function start(response) {
+var querystring = require("querystring");
+
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -8,7 +10,7 @@ function start(response) {
     '</head>'+
     '<body>'+
     '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<textarea name="text" rows="2" cols="40"></textarea>'+
     '<input type="submit" value="Submit text" />'+
     '</form>'+
     '</body>'+
@@ -22,7 +24,8 @@ function start(response) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Done: " + postData);
+  response.write("You've sent the text: "+
+  querystring.parse(postData).text);
   response.end();
 }
 
