e5b26b071089ed5685ab758c944540e01f8d202a
Jose Antonio Corachán <jacorachan@gmail.com>
2012-12-25 20:36:13 +0100

    added for to start, upload handles text from start

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
- function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+	function onRequest(request, response) {
+	    var postData = "";
+	    var pathname = url.parse(request.url).pathname;
+	    console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
-  }
+	    request.setEncoding("utf8");
 
-  http.createServer(onRequest).listen(process.env.VMC_APP_PORT || 8000, null);
-  console.log("Server has started.");
+	    request.addListener("data", function(postDataChunk) {
+	      postData += postDataChunk;
+	      console.log("Received POST data chunk '"+
+	      postDataChunk + "'.");
+	    });
+
+	    request.addListener("end", function() {
+	      route(handle, pathname, response, postData);
+	    });
+
+	  }
+
+	http.createServer(onRequest).listen(process.env.VMC_APP_PORT || 8000, null);
+	console.log("Server has started.");
 }
 
 exports.start = start;
